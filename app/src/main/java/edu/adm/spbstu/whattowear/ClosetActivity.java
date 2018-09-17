7af9f0196f2815eab392b4e4ceac7618f63460dc
polinaguryeva <gurevapolina98@gmail.com>
2018-09-17 20:52:20 +0300

    add alert for choosing gender change layout

--- app/src/main/java/edu/adm/spbstu/whattowear/ClosetActivity.java
+++ app/src/main/java/edu/adm/spbstu/whattowear/ClosetActivity.java
@@ -1,36 +1,42 @@
 package edu.adm.spbstu.whattowear;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.concurrent.ExecutionException;
 
 public class ClosetActivity extends AppCompatActivity {
 
     private ConstraintLayout constraintLayout;
     TextView city, temperature, precip;
     String currCity = "Samara";
+    boolean isWoman = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         loadViews();
+        showGenderAlert();
         updateWeather();
+
+    }
+
+    void showGenderAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Choose gender, please");
+        String[] items = {"male", "female"};
+        builder.setItems(items, (dialog, which) -> {
+            switch (which) {
+                case 0: isWoman = false; break;
+                case 1: isWoman = true; break;
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     void updateWeather() {
@@ -44,7 +50,7 @@ public class ClosetActivity extends AppCompatActivity {
     void loadViews() {
         setContentView(R.layout.activity_closet);
         constraintLayout = findViewById(R.id.view2);
-        constraintLayout.setBackgroundColor(Color.parseColor("#CEF6F5"));
+        constraintLayout.setBackgroundColor(getResources().getColor(R.color.backgroundClothet));
         city = findViewById(R.id.city);
         temperature = findViewById(R.id.temperature);
         precip = findViewById(R.id.precip);
