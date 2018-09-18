2d701f56129caf319d16be07ee2e16b199ab363f
polinaguryeva <gurevapolina98@gmail.com>
2018-09-18 06:50:29 +0300

    add saving data to internal storage

--- app/src/main/java/edu/adm/spbstu/whattowear/ClosetActivity.java
+++ app/src/main/java/edu/adm/spbstu/whattowear/ClosetActivity.java
@@ -1,13 +1,21 @@
 package edu.adm.spbstu.whattowear;
 
-import android.annotation.SuppressLint;
+import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+
 public class ClosetActivity extends AppCompatActivity {
+
+    static final String fileName = "user_info";
 
     private ConstraintLayout constraintLayout;
     TextView city, temperature, precip;
@@ -19,10 +27,71 @@ public class ClosetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         loadViews();
-        showGenderAlert();
+        if (isFirstLaunching()) {
+            showGenderAlert();
+        } else {
+            getGender();
+        }
         updateWeather();
-
     }
+
+    void getGender() {
+        try {
+            FileInputStream fis = this.openFileInput(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis));
+            for ( String splitBySemicolon : bufferedReader.readLine().split(";")) {
+                String[] splitByColon = splitBySemicolon.split(":");
+
+                switch (splitByColon[0].trim()) {
+                    case "gender":
+                        switch (splitByColon[1].trim()) {
+                            case "male": isWoman = false; break;
+                            case "female": isWoman = true; break;
+                            default: System.out.println("error parse file");
+                        }
+                        break;
+
+                    case "city":
+                        currCity = splitByColon[1].trim();
+                        break;
+                    default:
+                        System.out.println("error parse file");
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    boolean isFirstLaunching() {
+
+        File file = this.getFileStreamPath(fileName);
+        return !file.exists();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FileOutputStream fos;
+        String fileContent;
+        if (isWoman) {
+            fileContent = "city:" + currCity + "; gender: female";
+        } else {
+            fileContent = "city:" + currCity + "; gender: male";
+        }
+
+        try {
+            fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+            fos.write(fileContent.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 
     void showGenderAlert() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
